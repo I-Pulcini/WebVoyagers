1307c2969d9296083c4b6b5cdb7780fd39736393
Baldu21 <146322684+Baldu21@users.noreply.github.com>
2026-05-24 19:40:04 +0200

    Update LoginView.vue

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,81 +1,78 @@
 <script setup>
-import { ref } from 'vue'  //importo ref da Vue, ci serve per creare variabili reattive, che cambiano il lavoro
-import { useRouter } from 'vue-router'  //importiamo lo strumento di Router Vue di far viaggere l'utente tra le diverse pagine del sito usando il codice JavaScript
-import { userStore } from '../stores/userStore'  // importiamo lo store globale dell'utente 
+import { ref } from 'vue'  
+import { useRouter } from 'vue-router' 
+import { userStore } from '../stores/userStore'  
 
 
-const router = useRouter()  //abbiamo salvato il router in una variabile reattiva cosi da poterlo usare
+const router = useRouter()  
 
 
-const isLoginMode = ref(true)  //una variabile reattiva di tipo booleano, se è true  HTML mostrerà il modulo di login. se è false mostrerà il modulo di registrazione
+const isLoginMode = ref(true)  
 
 const username = ref('')
 const email = ref('')
 const password = ref('')
-//abbiamo scritto 3 variabili vuote che quando l'utente le scriverà nelle caselle di testo di riempiranno
-const messaggio = ref('')   //serve per stampare a schermo eventuali errori
-const mostraPassword = ref(false)  //variabile reattiva booleana: se true mostra la password in chiaro, se false la nasconde con i pallini
+const messaggio = ref('')   
+const mostraPassword = ref(false)  
 
 
 
-const toggleMode = () => {   // Abbiamo creato la funzione che cambia tra modalità Login e Registrazione
-  isLoginMode.value = !isLoginMode.value  //se è true diventa false e viceversa 
-  messaggio.value = ''  //svuotiamo i vecchi messaggi
-  password.value = ''   // Svuotiamo il campo della password
+const toggleMode = () => {  
+  isLoginMode.value = !isLoginMode.value  
+  messaggio.value = '' 
+  password.value = ''  
 }
 
-const gestisciSubmit = async () => {    // Abbiamo creato la funzione asincrona chiamata quando l'utente preme Accedi o Registrati
+const gestisciSubmit = async () => {   
   
-  const url = isLoginMode.value ? '/api/login' : '/api/register'  //se isLoginMode è vero,l'url da chiamare è /login altrimenti è /register
+  const url = isLoginMode.value ? '/api/login' : '/api/register'  
   
   
-  const payload = isLoginMode.value //iniziamo la costruzione del pacchetto di dati da spedire tramite la rete
-    ? { email: email.value, password: password.value }  // se stiamo nel Login i pacchetto avrà qeuste cose
-    : { username: username.value, email: email.value, password: password.value }  //se stiamo in modalità Registrazione
+  const payload = isLoginMode.value 
+    ? { email: email.value, password: password.value }  
+    : { username: username.value, email: email.value, password: password.value }  
 
   try {
    
-    const response = await fetch(url, {   //await blocca l'esecuzione di questa funzione, aspettadno che il server risponda
-      method: 'POST',   // è una chiamata di tipo post perche stimao inviando dati sicuri al server
-      headers: { 'Content-Type': 'application/json' },  //stiamo avvisando il server Node.js che il pacco in formato JSON sta arrivando
-      credentials: 'include',  // Abbiamo aggiunto questa opzione per inviare e ricevere i cookie di sessione
-      body: JSON.stringify(payload)  //trasformaimo in una stringa il testo JSON pronta per viaggiare su internet
+    const response = await fetch(url, {   
+      method: 'POST',   
+      headers: { 'Content-Type': 'application/json' },  
+      credentials: 'include', 
+      body: JSON.stringify(payload)  
     })
 
   
-    const data = await response.json()    // Abbiamo trasformato la risposta JSON ricevuta dal server in oggetto JavaScript
-
-    if (response.ok) {   //se la chiamata è andata a buon fine 
-      messaggio.value = data.message   //mostriamo a schermo il messaggio di successo mandato dal tuo file server.js
+    const data = await response.json()    
+    if (response.ok) {   
+      messaggio.value = data.message   
       
-      if (isLoginMode.value) {  //se abbiamo fatto il Login
+      if (isLoginMode.value) {  
        
-        userStore.setUser(data.userId, data.username)  //aggiorniamo il payload salvandoci ID e l'username dell'utente
+        userStore.setUser(data.userId, data.username)  
         
-        // Abbiamo aspettato 1 secondo per far leggere il messaggio, poi reindirizzato alla home
         setTimeout(() => router.push('/'), 1000)
 
-// quando il nostro utente decide di accedere o registrarsi al nostro sito web, le opzioni sono due. Se entriamo nel file LoginView.vue, possiamo notare che abbiamo la funzione fetch(url). Qui usiamo la parola chiave await che congela momentaneamente l'esecuzione di questa funzione all'interno del componente, aspettando che il server risponda, trattandosi di un'operazione asincrona di rete. Questo URL dipende dallo stato dell'utente: se l'utente è già registrato nei nostri archivi, l'URL punterà a /api/login, portandoci all'API numero 4 del server. Se invece non ha mai creato un account, punterà a /api/register.Analizziamo il caso in cui l'utente sia già registrato e debba semplicemente fare l'accesso.
+// quando l' utente decide di accedere o registrarsi al nostro sito web, le opzioni sono due. Se entriamo nel file LoginView.vue, possiamo notare che abbiamo la funzione fetch(url). Qui usiamo la parola chiave await che congela momentaneamente l'esecuzione di questa funzione all'interno del componente, aspettando che il server risponda, trattandosi di un'operazione asincrona di rete. Questo URL dipende dallo stato dell'utente: se l'utente è già registrato nei nostri archivi, l'URL punterà a /api/login, portandoci all'API numero 4 del server. Se invece non ha mai creato un account, punterà a /api/register.Analizziamo il caso in cui l'utente sia già registrato e debba semplicemente fare l'accesso.
 //Il client invia questa richiesta tramite fetch. Quando questo pacchetto arriva al server nel file server.js, l'API 4 intercetta i dati. All'interno dell'oggetto req.body (il Request Body) troviamo l'email e la password inserite in chiaro nel form. A questo punto, il server comunica direttamente e manualmente con il database PostgreSQL, in particolare con la tabella utenti, per cercare la riga in cui l'email corrisponde a quella ricevuta. Se il database restituisce zero righe e non trova nessuna corrispondenza, l'API si interrompe restituendo un errore. Se invece l'utente viene trovato, passiamo al controllo della password. La password digitata nel form è in chiaro, mentre quella memorizzata nel database è una stringa cifrata irreversibile. Per confrontarle, utilizziamo la funzione bcrypt.compare() che verifica se la password digitata coincide con l'hash del database. Se il controllo fallisce, il server restituisce un errore.
 // Se invece il controllo ha successo, il login va a buon fine ed entra in gioco il middleware dell'API 2 per la gestione dello stato: valorizzando le proprietà req.session.userId, req.session.username e req.session.isAdmin, il middleware genera una sessione persistente sul database e crea un cookie crittografato contenente l'ID di sessione. Tutte queste informazioni di profilo vengono incapsulate dentro res.json() in formato JSON. Questo pacchetto, che contiene il messaggio di successo, l'ID, lo username e il flag admin, viene rispedito indietro insieme al cookie appena generato dal middleware.
 // Il pacchetto viaggia dal server al client sotto forma di Response HTTP. A questo punto si riapre la logica di LoginView.vue: il codice riceve la risposta, controlla che sia andata a buon fine (response.ok), estrae i dati e invoca lo store globale per salvare l'ID e lo username dell'utente. Infine, dopo un timer di 1 secondo inserito per dare il tempo di leggere il messaggio di successo, il router reindirizza automaticamente l'utente alla Homepage
      
-      } else {  //altrimenti abbiamo fatto la registrazione
+      } else {  
        
-        setTimeout(() => {  //dopo 1,5 secondi cambiamo la visualizzazione passando al forma di login
+        setTimeout(() => {  
           isLoginMode.value = true
           messaggio.value = 'Ora puoi effettuare il login!'
         }, 1500)
 
         // Nel file login view.vue abbiamo una chiamata a fetch con scritto una variabile url. Questa variabile url dipende se l'utente ha fatto il login o se l'utente non l'ha mai fatto. Se l'utente non ha mai fatto il login, allora andiamo a finire nell'api register e quindi nell'api 3. Quindi abbiamo un collegamento client server. Il client invia nel body della richiesta il pacchetto JSON con i dati compilati, quindi username, email e password. E oltre al body abbiamo anche il cookie che è stato aggiunto dal browser. Successivamente, quando arriverà al server questo pacchetto, l'api 3 prende la password, la la cripta, trasformandola in password_hash e inserirà manualmente al database l'username, email e password cifrata. Se il database accetterà il nuovo utente, il server creerà una variabile response con stato 201 e inserisce in rest.json solo il messaggio di di successo, nient'altro, né l'username, né l'email, né la password, solo il messaggio di successo. Questo pacchetto è JSON con il messaggio di successo tornerà indietro al client, il pacchetto quindi tornerà in login view.vue sotto forma di response, se il response è ok, allora vuol dire che ci siamo registrati con successo e dopo 1,5 secondi cambierà automaticamente la visualizzazione passando in modalità login
       }
-    } else {  //se respond.ok è false allora ci sta un errore tipo che l'email è gia stata usata
+    } else {  
    
       messaggio.value = data.error   
     }
   } catch (err) {
 
-    messaggio.value = "Errore di connessione al server."   //salviamo un errore generico
+    messaggio.value = "Errore di connessione al server."   
   }
 }
 </script>
@@ -85,24 +82,24 @@ const gestisciSubmit = async () => {    // Abbiamo creato la funzione asincrona 
     <div class="login-box">  //riguardo bianco che contiene tutti i campi
       <h2>{{ isLoginMode ? 'Bentornato!' : 'Crea un Account' }}</h2>
       
-      <p v-if="messaggio" class="messaggio-alert">{{ messaggio }}</p>  //Mostra gli avvisi
+      <p v-if="messaggio" class="messaggio-alert">{{ messaggio }}</p> 
 
-      <form @submit.prevent="gestisciSubmit">  //serve per modificare il modulo di inserimento dati 
-        <div class="form-group" v-if="!isLoginMode">  //se la funzione è falsa cioè solo se l'utente si sta registrando
+      <form @submit.prevent="gestisciSubmit">  
+        <div class="form-group" v-if="!isLoginMode">  
           <label>Username</label>
           <input type="text" v-model="username" required placeholder="Scegli uno username">
         </div>
 
-        <div class="form-group">  //gruppo email
+        <div class="form-group"> 
           <label>Email</label>
           <input type="email" v-model="email" required placeholder="Inserisci la tua email">
         </div>
 
         <div class="form-group">
           <label>Password</label>
-          <div class="input-password-wrapper">  //creiamo un piccolo contenitore per posizionare l'icona dell'occhio sopra la casella di testo
+          <div class="input-password-wrapper"> 
             <input
-              :type="mostraPassword ? 'text' : 'password'"   //: type indica il Data Binding, se la var.reattiva mostraPassword è vera allora l'input diventa text ed è visibile se è falsa rimane password ed è coperta
+              :type="mostraPassword ? 'text' : 'password'"   
               v-model="password"
               required
               placeholder="Inserisci la password"
@@ -128,9 +125,9 @@ const gestisciSubmit = async () => {    // Abbiamo creato la funzione asincrona 
         </button>
       </form>
 
-      <p class="toggle-text">  //paragrafo dedicato al cambio schermate
+      <p class="toggle-text">  
         {{ isLoginMode ? 'Non hai un account?' : 'Hai già un account?' }}
-        <span @click="toggleMode" class="toggle-link">  // se si clicca si invertono le modalità della pagina e si svuota tutto
+        <span @click="toggleMode" class="toggle-link"> 
           {{ isLoginMode ? 'Registrati qui' : 'Accedi qui' }}
         </span> //contenitore generico in-line, non va a capo
       </p>
@@ -229,7 +226,7 @@ const gestisciSubmit = async () => {    // Abbiamo creato la funzione asincrona 
 
 .input-password-wrapper input {
   width: 100%;
-  padding-right: 42px;  /* spazio per l'icona */
+  padding-right: 42px;  
 }
 
 .occhio {

3316f3004b38357c06f60e754ea1f01ba903a412
Baldu21 <146322684+Baldu21@users.noreply.github.com>
2026-05-24 19:20:52 +0200

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-//IMPORTAZIONI PAGINE PRINCIPALI 
 import HomeView from '../views/HomeView.vue'
 import ErrorView from '../views/ErrorView.vue'
 import LoginView from '../views/LoginView.vue'
@@ -16,23 +14,21 @@ import LePrenotazioniView from '../views/LePrenotazioniView.vue'
 import ProfiloView from '../views/ProfiloView.vue'
 import AdminView from '../views/AdminView.vue'
 
-//  PAGINA DINAMICA UNICA PER TUTTI I VIAGGI 
-// Abbiamo sostituito tutte le vecchie pagine destinazione con questa unica pagina dinamica
-// che riceve l'id dall'URL e carica i dati dal backend.
+
 import DettaglioViaggioView from '../views/DettaglioViaggioView.vue'
 
-// Vue Router
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // rotte base
+    
     { path: '/', name: 'home', component: HomeView },
     { path: '/login', name: 'login', component: LoginView },
     { path: '/viaggi-disponibili', name: 'viaggi-disponibili', component: ViaggiDisponibiliView },
     { path: '/soldout', name: 'SoldOut', component: ViaggiSoldOutView },
     { path: '/viaggi-inarrivo', name: 'viaggi-inarrivo', component: ViaggiInArrivoView },
     
-    // rotte informazione ed utente 
+    
     { path: '/chi-siamo', name: 'chi-siamo', component: ChiSiamoView },
     { path: '/contattaci', name: 'contattaci', component: ContattaciView },
     { path: '/le-mie-prenotazioni', name: 'le-mie-prenotazioni', component: LePrenotazioniView },
@@ -45,10 +41,9 @@ const router = createRouter({
     { path: '/scopri-viaggio', name: 'scopri-viaggio', component: ScopriViaggioView },
 
     //  ROTTA DINAMICA PER TUTTI I VIAGGI 
-    // Abbiamo creato una sola rotta che funziona per tutti i viaggi del DB.
     { path: '/viaggio/:id', name: 'dettaglio-viaggio', component: DettaglioViaggioView },
 
-    //  ROTTA CATCH-ALL 
+    
     {
       // Se l'utente cerca un URL inesistente, Vue lo reindirizza a ErrorView 
       path: '/:pathMatch(.*)*',

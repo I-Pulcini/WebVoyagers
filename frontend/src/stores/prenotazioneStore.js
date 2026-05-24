bc900e9e8e05c06a5a9d6461fe29cb546e611445
Baldu21 <146322684+Baldu21@users.noreply.github.com>
2026-05-24 19:21:19 +0200

    Update prenotazioneStore.js

--- frontend/src/stores/prenotazioneStore.js
+++ frontend/src/stores/prenotazioneStore.js
@@ -1,10 +1,6 @@
 import { reactive } from 'vue'
 
-// STORE PRENOTAZIONE MISTERIOSA 
 
-// Abbiamo definito uno stato iniziale "vuoto" come oggetto costante.
-// Lo riutilizzeremo nella funzione reset() grazie allo spread operator (...)
-// che ci permette di copiare tutte le proprietà di un oggetto in un altro.
 const statoIniziale = {
     criteri: null,
     viaggioScelto: null,
@@ -13,11 +9,10 @@ const statoIniziale = {
 }
 
 export const prenotazioneStore = reactive({
-    // Abbiamo usato lo spread operator (...) per copiare tutte le proprietà
- 
+    
     ...statoIniziale,
 
-    // Abbiamo creato una funzione per salvare tutti i dati della prenotazione
+   
     set(criteri, viaggioScelto, viaggiSimili, codice) {
         this.criteri = criteri
         this.viaggioScelto = viaggioScelto

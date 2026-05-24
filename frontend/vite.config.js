ec0eb111a8629b2b1e4518e4fb81e7f201f11b91
Baldu21 <146322684+Baldu21@users.noreply.github.com>
2026-05-24 19:26:44 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -12,15 +12,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // CONFIGURAZIONE PROXY 
-  // Reindirizza tutte le chiamate che iniziano con '/api' verso il server effettivo 
+ 
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Inserire qui la porta del vostro server effettivo 
+        target: 'http://localhost:3000', 
         changeOrigin: true,
-        secure: false,                    // Disattiva la verifica HTTPS 
-        cookieDomainRewrite: 'localhost'  // Riscrive il dominio del cookie così il browser lo accetta correttamente
+        secure: false,                    
+        cookieDomainRewrite: 'localhost' 
       }
     }
   }
